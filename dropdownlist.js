589cf5a8dbdd9ca86b4ee9ab77397ddfae698a23
j <j@js-MacBook-Pro.local>
2019-04-15 21:18:26 -0400

    list

--- dropdownlist.js
+++ dropdownlist.js
@@ -2,11 +2,26 @@ function populate(s1,s2){
     var s1 = document.getElementById(s1);
     var s2 = document.getElementById(s2);
     s2.innerHTML = "";
+    if(s1.value == "Acura") {
+        var optionArray = ["|Model", "ILX|ILX","MDX|MDX","RDX|RDX","TLX|TLX"
+    ];
+    }
+    if(s1.value == "Cadillac") {
+        var optionArray = ["|Model","ATS|ATS","CTS|CTS","CT6|CT6", 
+        "Escalade|Escalade",
+        "XTS|XTS","XT4|XT4","XT5|XT5"
+    ];
+    }
     if(s1.value == "Chevy") {
         var optionArray = ["|Model", "Bolt|Bolt","Camero|Camaro","Corvette|Corvette","Cruze|Cruze","Equinox|Equinox","Impala|Impala","Malibu|Malibu",
         "Sonic|Sonic","Spark|Spark","Suburban|Suburban",
         "Tahoe|Tahoe","Traverse|Traverse","Trax|Trax",
         "Volt|Volt"
+    ];
+    }
+    if(s1.value == "Dodge") {
+        var optionArray = ["|Model", "Charger|Charger","Challenger|Challenger","Durango|Durango",
+        "Journey|Journey"
     ];
     }
     if(s1.value == "Ford") {
@@ -17,12 +32,65 @@ function populate(s1,s2){
         "Taurus|Taurus"
     ];
     }
+    if(s1.value == "GMC") {
+        var optionArray = ["|Model", "Acadia|Acadia","Terrain|Terrain",
+        "Yukon|Yukon"
+    ];
+    }
+    if(s1.value == "Hyundai") {
+        var optionArray = ["|Model", "Accent|Accent","Elantra|Elantra",
+        "Ioniq|Ioniq",
+        "Kona|Kona",
+        "Santa Fe|Santa Fe","Sonata|Sonata",
+        "Tuscon|Tuscon",
+        "Veloster|Veloster"
+    ];
+    }
+    if(s1.value == "Infiniti") {
+        var optionArray = ["|Model","Q50|Q50","Q60|Q60","Q70|Q70",
+        "QX30|QX30", "QX50|QX50","QX60|QX60","QX70|QX70","QX80|QX80"
+    ];
+    }
     if(s1.value == "Jeep") {
-        var optionArray = ["|Model", "C-Max|C-Max","EcoSport|EcoSport","Edge|Edge","Escape|Escape",
-        "Expedition|Expedition","Explorer|Explorer",
-        "Flex|Flex","Focus|Focus","Fiesta|Fiesta","Fusion|Fusion",
-        "Mustang|Mustang",
-        "Taurus|Taurus"
+        var optionArray = ["|Model", "Cherokee|Cherokee","Compass|Compass",
+        "Grand Cherokee|Grand Cherokee","Renegade|Renegade",
+        "Wrangler|Wrangler"
+    ];
+    }
+    if(s1.value == "Kia") {
+        var optionArray = ["|Model", "Cadenza|Cadenza","Forte|Forte","Niro|Niro",
+        "Optima|Optima","Rio|Rio",
+        "Sorento|Sorento","Soul|Soul","Sportage|Sportage","Stinger|Stinger"
+    ];
+    }
+    if(s1.value == "Lexus") {
+        var optionArray = ["|Model", "ES|ES","GS|GS","GX|GX","IS|IS",
+        "LS|LS",
+        "NX|NX",
+        "RC|RC","RX|RX"
+    ];
+    }
+    if(s1.value == "Mazda") {
+        var optionArray = ["|Model", "CX-3|CX-3","CX-5|CX-5","CX-9|CX-9",
+        "Mazda 3|Mazda 3","Mazda 6|Mazda 6","MX-5 Miata|MX-5 Miata"
+    ];
+    }
+    if(s1.value == "Porsche") {
+        var optionArray = ["|Model", "Cayenne|Cayenne","Macan|Macan",
+        "Panamera|Panamera","718|718","911|911"
+    ];
+    }
+    if(s1.value == "Subaru") {
+        var optionArray = ["|Model", "Ascent|Ascent","BRZ|BRZ","CrossTrek|CrossTrek",
+        "Forester|Forester",
+        "Impreza|Impreza","Legacy|Legacy",
+        "Outback|Outback",
+        "WRX|WRX"
+    ];
+    }
+    if(s1.value == "Tesla") {
+        var optionArray = ["|Model", "Model S|Model S","Model X|Model X",
+        "Model 3|Model 3"
     ];
     }
     if(s1.value == "Toyota") {
@@ -33,6 +101,18 @@ function populate(s1,s2){
         "Yaris|Yaris",
         "4Runner|4Runner",
         "86 / FR-S|86 / FR-S"
+    ];
+    }
+    if(s1.value == "Volkswagen") {
+        var optionArray = ["|Model","Atlas|Atlas","Beetle|Beetle",
+        "Golf|Golf",
+        "Jetta|Jetta","Passat|Passat",
+        "Tiguan|Tiguan"
+    ];
+    }
+    if(s1.value == "Volvo") {
+        var optionArray = ["|Model","S60|S60","S90|S90",
+        "XC40|XC40","XC60|XC60","XC90|XC90"
     ];
     }
     if(s1.value == "Nissan") {
