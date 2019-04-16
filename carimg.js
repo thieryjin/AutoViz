3829f5ac2afd6a562d15fd847a1e743ce1cc8498
j <j@js-MacBook-Pro.local>
2019-04-16 06:55:37 -0400

    figure

--- carimg.js
+++ carimg.js
@@ -1,14 +1,36 @@
 
 
 function update(val) {
-    if(val === "A4") {
-        alert("The input value has changed. The new value is: " + val);
+    
+        /*alert("The input value has changed. The new value is: " + val);
         var dd1 = document.getElementById("d1");
-        dd1.style.backgroundColor="lime";
-    }
-    if(val === "A5") {
+        dd1.style.backgroundColor="lime";*/
+    var filepath = "pic/"+val+".jpg";
+    var img= new SimpleImage(filepath);
+        var dd1 = document.getElementById("d1");
+        img.drawTo(dd1);
+    /*if(val === "A5") {
         var img= new SimpleImage("pic/A5.jpg")
         var dd1 = document.getElementById("d1");
         img.drawTo(dd1);
-    }
+    }*/
 }
+window.onload=function() {
+    document.getElementById("slct5").onchange=function() {
+        var x = this.value;
+        var filepath = "pic/"+x+".jpg";
+    var img= new SimpleImage(filepath);
+        var dd2 = document.getElementById("d2");
+        img.drawTo(dd2);
+    }
+    }
+
+window.onload=function() {
+    document.getElementById("slct5").onchange=function() {
+        var x = this.value;
+        var filepath = "pic/"+x+".jpg";
+    var img= new SimpleImage(filepath);
+        var dd2 = document.getElementById("d2");
+        img.drawTo(dd2);
+    }
+    }
